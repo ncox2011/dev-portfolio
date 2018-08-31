acc9b6b181375f395cf767086e7c374b45d81eb0
Natasha Cox <natashac1014@gmail.com>
2018-08-31 14:26:19 -0500

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div id="footer">
+                <div className="inner">
+                    <ul className="icons">
+                        <li><a href="https://www.linkedin.com/in/ncox2011/" className="icon fa-linkedin"><span className="label">Twitter</span></a></li>
+                        <li><a href="https://github.com/ncox2011" className="icon fa-github"><span className="label">Github</span></a></li>
+                        <li><a href="#" className="icon fa-envelope-o"><span className="label">Email</span></a></li>
+                    </ul>
+                    <ul className="copyright">
+                        <li>&copy; Gatsby Starter Strata</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+import Footer from './Footer'
+import avatar from '../assets/images/avatar.jpg'
+
+class Header extends React.Component {
+    render() {
+        return (
+            <header id="header">
+                <div className="inner">
+                    <a href="#" className="image avatar"><img src="https://scontent-atl3-1.xx.fbcdn.net/v/t1.0-9/11018778_10155211946185570_1623082764662492657_n.jpg?_nc_cat=0&oh=5df62aedfcf580f897b6a73facf222ea&oe=5C2F6227" alt="" /></a>
+                    <h1><strong>I am Natasha</strong>, lover of coffee, wine, a good book, 2 rascals, food, and coding!!<br />
+                    Current favorite developer tools include ReactJs and C#.<br />
+                    </h1>
+                </div>
+                <Footer />
+            </header>
+        )
+    }
+}
+
+export default Header
